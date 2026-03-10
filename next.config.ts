b884066a1b54c3ab3fb35697b88d418bc354c372
SaranchaiZa <115490161+JEngHAng@users.noreply.github.com>
2026-03-10 10:37:39 +0700

    commit 2 setting all

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  //  configure the remote image domains
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mhjoggvvhlwppewzocjr.supabase.co",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn-icons-png.flaticon.com",
+        port: "",
+        pathname: "/**",
+      }
+    ],
+  },
 };
 
 export default nextConfig;
